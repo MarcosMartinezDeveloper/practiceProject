f8274037afe6dbf0b25b4b58753c4d2f83e9866d
marcos <marcos@SATELLITE-PRO-C50-J>
2026-09-26 16:39:17 +0200

    UserService tested with mockito

--- practiceProject/src/test/java/com/marcosmartinezdeveloper/practiceProject/facade/services/UserServiceTest.java
+++ practiceProject/src/test/java/com/marcosmartinezdeveloper/practiceProject/facade/services/UserServiceTest.java
@@ -3,14 +3,18 @@ package com.marcosmartinezdeveloper.practiceProject.facade.services;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.marcosmartinezdeveloper.practiceProject.facade.dtos.LoginDTO;
 import com.marcosmartinezdeveloper.practiceProject.facade.dtos.SignUpDTO;
 import com.marcosmartinezdeveloper.practiceProject.facade.mappers.SignUpDTOMapperImpl;
 import com.marcosmartinezdeveloper.practiceProject.persistence.entities.User;
@@ -35,6 +39,8 @@ public class UserServiceTest {
 	static String password;
 	static String email;
 	static String role;
+	static User user;
+	static Optional<User> optionalUser;
 	
 	@BeforeAll
 	public static void setUp() {
@@ -43,6 +49,8 @@ public class UserServiceTest {
 		password = "aWell_formedPassword1";
 		email = "aValid@gmail.com";
 		role = "CUSTOMER";
+		user = new User(null, username, password, email, role);
+		optionalUser = Optional.ofNullable(user);
 	}
 	
 	@Test
@@ -60,14 +68,29 @@ public class UserServiceTest {
 		assertThat(newUser).isEqualTo(user);
 	}
 	
-//	@Test
-//	public void logIn() {
-//		
-//	}
-//	
-//	@Test
-//	public void loadUserByUsername() {
-//		
-//	}
+	@Test
+	public void logIn() {
+		LoginDTO loginDTO = new LoginDTO(username, password);
+
+		given(userRepository.findByUsername(username)).willReturn(optionalUser);
+		given(passwordEncoder.matches(password, password)).willReturn(true);
+
+		boolean result = userService.logIn(loginDTO);
+		
+		assertThat(result).isNotNull();
+		assertThat(result).isTrue();
+	}
+	
+	@Test
+	public void loadUserByUsername() {
+		UserDetails userDetails = (UserDetails) user;
+
+		given(userRepository.findByUsername(username)).willReturn(optionalUser);
+
+		UserDetails userDetailsRecieved = userService.loadUserByUsername(username);
+		
+		assertThat(userDetailsRecieved).isNotNull();
+		assertThat(userDetailsRecieved).isEqualTo(userDetails);
+	}
 
 }
